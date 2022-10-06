3c993568fd32f80c5f52ddfecc2d6cb3cc9d5253
Marten Kaffler <marten.kaffler@bayer.com>
2022-10-06 17:40:16 +0200

    added layout for login

--- client/src/routes/definitions/submissions/+page.svelte
+++ client/src/routes/definitions/submissions/+page.svelte
@@ -5,7 +5,45 @@
 
 <Header />
 
-<h1>Here you will be able to send in definitions.</h1>
+<div class="flex bg-sky-500 h-full justify-center p-4">
+  <div class="card  grid justify-center w-full shadow-2xl bg-base-100">
+    <form class="card-body w-fit md:w-screen" action="./">
+      <div class="form-control grid grid-cols-3 gap-4 md:px-8">
+        <label class="label">
+          <span class="label-text">Author</span>
+        </label>
+        <input type="text" placeholder="Author name..." class="input input-bordered col-span-2" required/>
+      </div>
+      <div class="form-control grid grid-cols-3 gap-4 md:px-8">
+        <label class="label">
+          <span class="label-text">Source</span>
+        </label>
+        <input type="text" placeholder="Source..." class="input input-bordered col-span-2" required/>
+      </div>
+      <div class="form-control grid grid-cols-3 gap-4 md:px-8">
+        <label class="label">
+          <span class="label-text">Year of Submission</span>
+        </label>
+        <input type="number" min="1000" max="2099" step="1" placeholder="Year of Submission..." class="input input-bordered col-span-2" required/>
+      </div>
+      <div class="form-control grid grid-cols-3 gap-4 md:px-8">
+        <label class="label">
+          <span class="label-text">Category</span>
+        </label>
+        <input type="text" placeholder="Catergory..." class="input input-bordered col-span-2" required/>
+      </div>
+      <div class="form-control grid grid-cols-3 gap-4 md:px-8">
+        <label class="label">
+          <span class="label-text">Definition</span>
+        </label>
+        <textarea placeholder="Difinition..." class="input input-bordered col-span-2 h-40" />
+      </div>
+      <div class="form-control mt-6 grid grid-cols-3 lg:grid-cols-5 gap-4 md:px-8">
+        <input type="submit" class=" btn btn-primary justify-center col-start-3 lg:col-start-5" value="Submit">
+      </div>
+    </form>
+  </div>
+</div>
 
 <Footer />
 
--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -5,37 +5,41 @@
 
 <Header />
 
-<div class="hero min-h-screen bg-base-200">
-	<div class="hero-content flex-col lg:flex-row-reverse">
-		<div class="text-center lg:text-left">
-			<h1 class="text-5xl font-bold">Login now!</h1>
-			<p class="py-6">
-				Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-				quasi. In deleniti eaque aut repudiandae et a id nisi.
-			</p>
-		</div>
-		<div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-			<div class="card-body">
-				<div class="form-control">
-					<label class="label">
-						<span class="label-text">Email</span>
-					</label>
-					<input type="text" placeholder="email" class="input input-bordered" />
-				</div>
-				<div class="form-control">
-					<label class="label">
-						<span class="label-text">Password</span>
-					</label>
-					<input type="text" placeholder="password" class="input input-bordered" />
-					<label class="label">
-						<a href="#" class="label-text-alt link link-hover">Forgot password?</a>
-					</label>
-				</div>
-				<div class="form-control mt-6">
-					<button class="btn btn-primary">Login</button>
-				</div>
+<div class="flex flex-col h-full justify-center items-center p-4">
+	<p class="text-2xl font-bold pb-4">Login to YACOID!</p>
+	<!--<p class="text-lg font-medium">To add definitions you need an account</p>-->
+	<div class="card grid place-items-center w-3/4 sm:w-1/2 xl:w-1/4 shadow-2xl bg-base-100">
+		<form class="card-body" action="./">
+			<div class="form-control grid grid-cols-3 gap-4 md:px-8">
+				<label class="label">
+					<span class="label-text">Email</span>
+				</label>
+				<input
+					type="text"
+					placeholder="Email..."
+					class="input input-bordered col-span-2"
+					required
+				/>
 			</div>
-		</div>
+			<div class="form-control grid grid-cols-3 gap-4 md:px-8">
+				<label class="label">
+					<span class="label-text">Password</span>
+				</label>
+				<input
+					type="password"
+					placeholder="Password..."
+					class="input input-bordered col-span-2"
+					required
+				/>
+			</div>
+			<div class="form-control mt-6 grid grid-cols-3 lg:grid-cols-5 gap-4 md:px-8">
+				<input
+					type="submit"
+					class=" btn btn-primary justify-center col-start-3 lg:col-start-5"
+					value="Login"
+				/>
+			</div>
+		</form>
 	</div>
 </div>
 
